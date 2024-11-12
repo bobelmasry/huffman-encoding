dae7b899013dc509b94b0733519dd6a33f3d5e1d
Aly <alyhassan2254@gmail.com>
2024-11-12 15:46:01 +0200

    added frequencies of characters

--- main.cpp
+++ main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <map>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    ifstream inputFile("inputText.txt");
+    map<char, int> letterFrequency;
+    string line;
+
+    if (!inputFile) {
+        cout << "Error opening Input file" << endl;
+        return 1;
+    }
+
+    while (getline(inputFile, line)) {
+        for (char ch : line) {
+            if (isalpha(ch)) {
+                ch = tolower(ch);
+                letterFrequency[ch]++;
+            }
+        }
+    }
+    inputFile.close();
+
+    for (const auto &pair : letterFrequency) {
+        cout << pair.first << ": " << pair.second << endl;
+    }
+
+    ofstream outputFile("outputFrequency.txt");
+    if (outputFile) {
+        for (const auto &pair : letterFrequency) {
+            outputFile << pair.first << ": " << pair.second << endl;
+        }
+        outputFile.close();
+    } else {
+        cout << "Error opening output file" << endl;
+    }
+
     return 0;
 }
