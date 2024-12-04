c22808b6b8a0dd5c2476717d821c34b4bcd7f756
NourElghaly <nourelghaly@aucegypt.edu>
2024-12-04 17:58:49 +0200

    priority queue

--- file_zipper/priorityqueue.cpp
+++ file_zipper/priorityqueue.cpp
@@ -1,29 +1,82 @@
-#include <QString>
 #include "priorityqueue.h"
-using namespace std;
+#include <stdexcept>
+
+template class priorityqueue<QString>;
 
 template<typename T>
-priorityqueue<T>::priorityqueue(int maxsize):capacity(maxsize+1), size(0) //max size is plus 1 because
-{arr = new T[capacity];}
+priorityqueue<T>::priorityqueue(int maxsize) : capacity(maxsize), size(0) {
+    arr = new T[capacity];
+}
 
 template<typename T>
-priorityqueue<T>::~priorityqueue()
-{delete [] arr;}
+priorityqueue<T>::~priorityqueue() {
+    delete[] arr;
+}
 
 template<typename T>
-void priorityqueue<T>::insert(const T& item)
-{upheap(item);}
-
-template<typename T>
-T priorityqueue<T>::remove()
-{
-    if(size==0)
-    {
-       arr[0]=arr[arr->size()];
-        upheap(size);
+void priorityqueue<T>::upheap(int index) {
+    while (index > 0) {
+        int parent = (index - 1) / 2;
+        if (arr[index] <= arr[parent]) {
+            break;
+        }
+        std::swap(arr[index], arr[parent]);
+        index = parent;
     }
 }
 
 template<typename T>
-void priorityqueue<T>::doubleCapacity()
-{}
+void priorityqueue<T>::doubleCapacity() {
+    capacity *= 2;
+    T* newArr = new T[capacity];
+    for (int i = 0; i < size; ++i) {
+        newArr[i] = arr[i];
+    }
+    delete[] arr;
+    arr = newArr;
+}
+
+template<typename T>
+void priorityqueue<T>::insert(const T& item) {
+    if (size == capacity) {
+        doubleCapacity();
+    }
+    arr[size] = item;
+    upheap(size);
+    ++size;
+}
+
+template<typename T>
+T priorityqueue<T>::remove() {
+    if (size == 0) {
+        throw std::underflow_error("Priority queue is empty.");
+    }
+    T maxItem = arr[0];
+    arr[0] = arr[size - 1];
+    --size;
+
+    int index = 0;
+    while (index < size) {
+        int leftChild = 2 * index + 1;
+        int rightChild = 2 * index + 2;
+        int largest = index;
+
+        if (leftChild < size && arr[leftChild] > arr[largest]) {
+            largest = leftChild;
+        }
+
+        if (rightChild < size && arr[rightChild] > arr[largest]) {
+            largest = rightChild;
+        }
+
+        if (largest == index) {
+            break;
+        }
+
+        swap(arr[index], arr[largest]);
+        index = largest;
+    }
+
+    return maxItem;
+}
+
